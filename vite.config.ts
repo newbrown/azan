37a28e1069bfc362b3776c17a89320b834b19d4d
Naswif Cohen <naswifcohen@naswifcohen.naswifcohen>
2025-11-23 06:38:06 +0200

    fix: relative asset paths and add schema

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: process.env.VITE_BASE || "/azan/",
   plugins: [react()],
   resolve: {
     alias: {
