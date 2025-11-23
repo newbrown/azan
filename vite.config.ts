f9e3cede9552250f0bf9c662d0e63fc98e723d56
Naswif Cohen <naswifcohen@naswifcohen.naswifcohen>
2025-11-23 06:47:54 +0200

    rebuild: relative base with root router for github pages root

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  base: process.env.VITE_BASE || "/azan/",
+  base: "./",
   plugins: [react()],
   resolve: {
     alias: {
